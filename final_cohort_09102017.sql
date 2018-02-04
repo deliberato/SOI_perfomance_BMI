101a869a93e1a22a7e15817d697839b9b68c93fd
Rodrigo Octávio deliberato <rodrigo2@mit.edu>
2018-02-04 20:40:15 -0200

    Update final_cohort_09102017.sql

--- final_cohort_09102017.sql
+++ final_cohort_09102017.sql
@@ -1,10 +1,14 @@
-SET search_path to eicu_crd,public;
 
-WITH t1 as
+-- ---------------------------------------------------------------------------------------
+-- Title: SQL Code extraction
+-- Description:  Please read "read me" for details about inclusion and exclusion criteria 
+-- SET search_path to eicu_crd,public;
+-- ---------------------------------------------------------------------------------------
+WITH t1 as -- extracting most of the variables
 (
 SELECT 
 	 a.patientunitstayid
-	, a.uniquepid,ROW_NUMBER() over (partition by a.uniquepid order by  a.hospitaladmityear ASC) AS position
+	, a.uniquepid, ROW_NUMBER() OVER (partition BY a.uniquepid ORDER BY  a.hospitaladmityear ASC) AS position
 	, a.hospitaladmityear
 	, a.age
         , case -- fixing age >89 to 93
@@ -59,73 +63,82 @@ SELECT
 	, c. actualiculos
 	, c.unabridgedhosplos as real_hospital_los
 	, c.actualhospitallos
-        , c.unabridgedactualventdays as ventduration,t.intubated as intubated_first_24h,(cv.sofa_cv+respi.sofa_respi+ renal.sofarenal+others.sofacoag+ others.sofaliver+others.sofacns) as sofatotal
-, c.predictedicumortality, c.actualicumortality, c.acutephysiologyscore
-,case -- fixing icu mortality
-      when lower(c.actualicumortality) like '%alive%' THEN 0
-      when lower(c.actualicumortality) like '%expired%' THEN 1 
-      else null end as died_icu
-,c.apachescore,c.apacheversion, o.oasis, o.oasis_prob,c.predictedhospitalmortality,c.actualhospitalmortality
-
-,b.diedinhospital
-from patient a
-left join apachepredvar b
-on a.patientunitstayid = b.patientunitstayid
-left join apachepatientresult c
-on a.patientunitstayid = c.patientunitstayid
-left join apacheapsvar t
-on a.patientunitstayid = t.patientunitstayid
-left join sofacv cv
-on a.patientunitstayid = cv.patientunitstayid
-left join sofarespi respi
-on a.patientunitstayid = respi.patientunitstayid
-left join sofarenal renal
-on a.patientunitstayid = renal.patientunitstayid
-left join sofa3others others
-on a.patientunitstayid = others.patientunitstayid
-left join charlson_score ch
-on a.patientunitstayid = ch.patientunitstayid
-left join oasis o
-on a.patientunitstayid = o.patientunitstayid
-left join diagnosis d
-on a.patientunitstayid = d.patientunitstayid
-left join hospital h
-on  a.hospitalid = h.hospitalid
-where a.age not in ( '0', '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15') -- excluding patients below 16 years
-and a.admissionheight is not null
-and a.admissionweight is not null
+        , c.unabridgedactualventdays as ventduration
+	, t.intubated as intubated_first_24h
+	, (cv.sofa_cv+respi.sofa_respi+ renal.sofarenal+others.sofacoag+ others.sofaliver+others.sofacns) as sofatotal
+        , c.predictedicumortality
+	, c.actualicumortality
+	, c.acutephysiologyscore
+        , CASE -- fixing icu mortality
+      WHEN lower(c.actualicumortality) LIKE '%alive%' THEN 0
+      WHEN lower(c.actualicumortality) LIKE '%expired%' THEN 1 
+      ELSE NULL END AS died_icu
+       , c.apachescore
+       , c.apacheversion
+       , o.oasis[
+       , o.oasis_prob
+       , c.predictedhospitalmortality
+       , c.actualhospitalmortality
+       , b.diedinhospital
+FROM patient a
+LEFT JOIN apachepredvar b
+     ON a.patientunitstayid = b.patientunitstayid
+LEFT JOIN apachepatientresult c
+     ON a.patientunitstayid = c.patientunitstayid
+LEFT JOIN apacheapsvar t
+     ON a.patientunitstayid = t.patientunitstayid
+LEFT JOIN sofacv cv
+     ON a.patientunitstayid = cv.patientunitstayid
+LEFT JOIN sofarespi respi
+     ON a.patientunitstayid = respi.patientunitstayid
+LEFT JOIN sofarenal renal
+     ON a.patientunitstayid = renal.patientunitstayid
+LEFT JOIN sofa3others others
+     ON a.patientunitstayid = others.patientunitstayid
+LEFT JOIN charlson_score ch
+     ON a.patientunitstayid = ch.patientunitstayid
+LEFT JOIN oasis o
+     ON a.patientunitstayid = o.patientunitstayid
+LEFT JOIN diagnosis d
+     ON a.patientunitstayid = d.patientunitstayid
+LEFT JOIN hospital h
+     ON  a.hospitalid = h.hospitalid
+WHERE
+     a.age not in ( '0', '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15') -- excluding patients below 16 years
+AND a.admissionheight IS NOT null
+AND  a.admissionweight IS NOT  null
 -- and a.dischargeweight is not null -- discharge weight has more NULL values than admission weight (90k vs 16k) so I decided to use admission weight.
-and diedinhospital is not null
-and b.readmit = 0 -- excluding readmission
-and c.predictedhospitalmortality != '-1'
-and c.apacheversion = 'IV'
-and  a.admissionweight between 30 and 320  -- excluding patients with weight < 30kg and more tha 320 kg -- I compared with dischargeweight and weigths outside this range didn´t make sense
-and a.admissionheight between  120 and 230 -- excluding patients with height less than 120 cm and more than 220 cm
-and lower(d.diagnosisstring) not like 'pregnancy'-- excluding patients with preganacy related diagnosis
+AND  diedinhospital IS NOT null
+AND b.readmit = 0 -- excluding readmission
+AND c.predictedhospitalmortality != '-1'
+AND c.apacheversion = 'IV'
+AND  a.admissionweight BETWEEN 30 AND  320  -- excluding patients with weight < 30kg and more tha 320 kg -- I compared with dischargeweight and weigths outside this range didn´t make sense
+AND a.admissionheight BETWEEN  120 AND  230 -- excluding patients with height less than 120 cm and more than 220 cm
+AND lower(d.diagnosisstring) NOT LIKE  'pregnancy'-- excluding patients with preganacy related diagnosis
 )
-, t2 as  -- creating bmi
+, t2 AS  -- creating body mass index variable
 (
-select t1.*, t1.weight/(t1.height*t1.height/10000) as bmi 
-from t1
+SELECT t1.*, t1.weight/(t1.height*t1.height/10000) AS bmi 
+FROM t1
 )
-, t3 as -- creating BMI category
+, t3 AS -- creating BMI category
 (
-select t2.*,
-case
-  when t2.bmi < 18.5 then 1
-  when t2.bmi >= 18.5 and t2.bmi < 24.999999999 then 2
-  when t2.bmi >= 25 and t2.bmi < 30 then 3
-  when t2.bmi >= 30 then 4
-  else 0 end as bmi_group 
-from t2
+SELECT t2.*,
+CASE
+  WHEN t2.bmi < 18.5 THEN 1 -- underweight
+  WHEN t2.bmi >= 18.5 AND t2.bmi < 24.999999999 then 2 -- normal weight
+  WHEN t2.bmi >= 25 AND t2.bmi < 30 then 3 -- overweight
+  WHEN t2.bmi >= 30 THEN 4 -- obese
+  ELSE 0 END AS bmi_group 
+FROM t2
 )
-select t3.*,
-case
-	when t3.bmi_group = 1 then 'underweight'
-	when t3.bmi_group = 2 then 'normal weight'
-	when t3.bmi_group = 3 then 'overweight'
-	when t3.bmi_group = 4 then 'obese'
-	else null end as bmi_category 
-from t3
-where position =1 -- including just the first ICU admission (according to hospital admission year)
--- and bmi_group = 4 -- or 2 or 3 or 4 -- to din the number of patients of each BMI category
+SELECT t3.*,
+CASE
+	WHEN t3.bmi_group = 1 THEN 'underweight'
+	WHEN t3.bmi_group = 2 THEN 'normal weight'
+	WHEN t3.bmi_group = 3 THEN 'overweight'
+	WHEN t3.bmi_group = 4 THEN 'obese'
+	ELSE null END AS bmi_category 
+FROM t3
+WHERE position =1 -- including just the first ICU admission (according to hospital admission year)
+
